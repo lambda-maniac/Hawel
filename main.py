80a328974245d824651b9732e47bdf1a910e1697
Lord Zarkares <73501736+LordZarkares@users.noreply.github.com>
2021-04-25 19:09:42 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,44 @@
+from token       import Token
+from lexer       import Lexer
+from nodes       import *
+from parser      import Parser
+from context     import *
+from classes     import *
+from interpreter import *
+from hawelTokens import TOKENS
+
+def main():
+
+    import sys
+    import json
+
+    # Setup Context
+    context_main = Context('Hawel')
+
+    context_main.symbolTable.symbols = \
+    {
+        'null': Int(0),
+        'pi'  : Int(3.14159265368979),
+        'echo': BuiltInPrint(),
+        'get' : BuiltInInput(),
+        'int' : BuiltInInt(),
+        'str' : BuiltInString(),
+    }
+
+    if len(sys.argv) == 1:
+        while True:
+
+            tokens = Lexer(TOKENS).lex(input("~%$ "))
+            print(f"\nToken List: {tokens}")
+
+            ast = Parser(tokens).parse()
+            print(f'\nAST:\n{json.dumps(eval(ast.__repr__()), indent = 4)}')
+
+            result = Interpreter(ast).interpretate(context_main)
+            print(f'\nresult: {result}')
+
+    else:
+        with open(f'{sys.argv[1]}', 'r') as file:
+            Interpreter(Parser(Lexer(TOKENS).lex(file.read())).parse()).interpretate(context_main)
+
+if __name__ == '__main__': main()
